38865b5be8e6b1cf85a9c698d41474a4b4750135
Jessica <Jessica@users-iMac.local>
2020-04-20 13:17:35 -0400

    PROJECT ROUTER

---
+++ projects/project-model.js
@@ -0,0 +1,46 @@
+const db = require('../data/db-config.js'); 
+
+module.exports = {
+  getProjects,
+  getProjectsById,
+  addProjects,
+  updateProjects,
+  removeProjects,
+};
+
+
+// ----------------------------------------------
+
+// CRUD operations for Projects: 
+
+// GET /api/projects - get projects master list
+function getProjects() {
+  return db('projects'); 
+}
+
+// GET /api/projects/:id - get a specific project
+function getProjectsById(id) {
+  return db('projects')
+        .where({id})
+        .first(); 
+} 
+
+// POST /api/projects - add a new project
+function addProjects(project) {
+  return db('projects')
+        .insert(project); 
+}
+
+// PUT /api/projects/:id - updates a project
+function updateProjects(changes, id) {
+  return db('projects')
+        .where({id})
+        .update(changes);
+}
+
+// DELETE /api/projects/:id - remove a given project
+function removeProjects(id) {
+  return db('projects')
+        .where({id})
+        .del();
+}
